dbbc22239f3ff5b211fb12a641ae7a1c6a62ecf1
BenOno <bentoki.1213@gmail.com>
2024-08-19 12:27:33 -0700

    add images remote con

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.dev.to",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
